3ef6beda5d515b3e8bc7eb422fb24f44d03364bd
Paul Johnson <pejohnson@etsy.com>
2020-11-30 12:20:34 -0800

    Setup for AoC 2020

---
+++ templates/a.py
@@ -0,0 +1,7 @@
+import os
+
+file_path = os.path.dirname(os.path.realpath(__file__))
+
+input_path = file_path + "/a.in.txt"
+
+inpt = open(input_path, 'r')
---
+++ templates/b.py
@@ -0,0 +1,7 @@
+import os
+
+file_path = os.path.dirname(os.path.realpath(__file__))
+
+input_path = file_path + "/b.in.txt"
+
+inpt = open(input_path, 'r')

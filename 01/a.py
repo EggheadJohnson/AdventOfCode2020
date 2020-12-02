da5290125490e5257b2f5291cc06c72c2e9f2126
Paul Johnson <PaulE.Johnson@gmail.com>
2020-12-01 20:57:30 -0800

    i think this all looks nice then i suppose

--- 01/a.py
+++ 01/a.py
@@ -1,15 +1,9 @@
-import os
 
-file_path = os.path.dirname(os.path.realpath(__file__))
+def a(input):
+    row_set = set()
 
-input_path = file_path + "/a.in.txt"
-
-inpt = open(input_path, 'r')
-
-row_set = set()
-
-for line in inpt:
-    line = int(line)
-    if 2020 - line in row_set:
-        print(line, 2020 - line, line * (2020 - line))
-    row_set.add(line)
+    for line in input:
+        line = int(line)
+        if 2020 - line in row_set:
+            return line, 2020 - line, line * (2020 - line)
+        row_set.add(line)
